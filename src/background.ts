b40ebdd951beb6d6bb7da326e7b3be20f6f446f1
Arnold Chand <creativenull@outlook.com>
2021-06-01 00:18:03 -0400

    feat(ext): added more code to test messages between tabs and the runtime

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,31 @@
+interface BlockerStore {
+  blocklist: string[]
+}
+
+const store: BlockerStore = {
+  blocklist: ['developer.chrome.com'],
+}
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log("The color is green.");
-  });
+  chrome.runtime.onMessageExternal.addListener((msg) => {
+    // if (msg === 'overwrite') {
+    //   chrome.tabs.executeScript({
+    //     code: "document.body.innerHTML = 'Yooo'",
+    //   })
+    // }
 
-  chrome.webNavigation.onCompleted.addListener(() => {
-    console.log('Completed the nabigation')
-  }, {url: [{urlMatches : 'https://developer.chrome.com'}]})
-});
+    console.log('runtime.onMessage', msg)
+  })
 
+  chrome.webNavigation.onBeforeNavigate.addListener((details) => {
+    console.log('onBeforeNavigate', details)
+    const url = details.url
+    for (const blockedUrl of store.blocklist) {
+      if (url.includes(blockedUrl)) {
+        console.log('Found on blocklist', blockedUrl)
+        chrome.runtime.sendMessage({ message: 'Hello World' })
+        // chrome.tabs.sendMessage(details.tabId, { message: 'Hello World' })
+      }
+    }
+  })
+})
